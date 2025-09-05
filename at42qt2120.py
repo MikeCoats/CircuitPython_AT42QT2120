9302cb6ce391bc449030b75c4217253c9d73efb3
Mike Coats <i.am@mikecoats.com>
2025-09-05 22:50:00 +0100

    Update the version number for PyPI

--- at42qt2120.py
+++ at42qt2120.py
@@ -5,7 +5,7 @@ import busio
 from adafruit_bus_device import i2c_device
 from micropython import const
 
-__version__ = "0.0.0+auto.0"
+__version__ = "1.0.0.alpha.3"
 __repo__ = "https://github.com/MikeCoats/CircuitPython_at42qt2120.git"
 
 AT42QT2120_I2CADDR_DEFAULT = const(0x1C)
